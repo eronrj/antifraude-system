19736810638b1354b5b48024e7d2cc67334ee41a
eronrj <eronrj21@gmail.com>
2026-05-25 15:15:37 -0300

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,1 +1,23 @@
+CREATE TABLE teams (
 
+    id SERIAL PRIMARY KEY,
+
+    name VARCHAR(100),
+
+    league VARCHAR(50),
+
+    attack_rating NUMERIC,
+
+    defense_rating NUMERIC
+
+);
+
+CREATE TABLE matches (
+
+    id SERIAL PRIMARY KEY,
+
+    home_team_id INTEGER,
+
+    away_team_id INTEGER
+
+);
