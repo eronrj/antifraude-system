bf4070b1bbb5177ee03ed41a42bca05218cecb63
eronrj <eronrj21@gmail.com>
2026-05-25 15:29:43 -0300

    Implement Poisson probability calculation

--- backend/src/main/java/com/antiblefe/service/PoissonService.java
+++ backend/src/main/java/com/antiblefe/service/PoissonService.java
@@ -1,1 +1,24 @@
+package com.antiblefe.service;
 
+public class PoissonService {
+
+    public double probability(
+            int goals,
+            double lambda){
+
+        return Math.pow(lambda, goals)
+                * Math.exp(-lambda)
+                / factorial(goals);
+    }
+
+    private long factorial(int n){
+
+        long f = 1;
+
+        for(int i = 2; i <= n; i++){
+            f *= i;
+        }
+
+        return f;
+    }
+}
